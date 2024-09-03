8eb8a6a3edeeb24db70c821e3bc7dec57f99210a
Hashir Ali Ahmad <hashir.47a@gmail.com>
2024-09-03 11:21:42 +0500

    build done

--- src/app/coming/page.tsx
+++ src/app/coming/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <div className="w-full h-screen bg-black flex items-center justify-center flex-col">
       <Image className="rounded-lg" height={300} width={500} src="/coming.png" alt="" />
       <h1 className="font-semibold m-4 text-5xl text-lime-300">Coming Soon</h1>
-      <p className="text-lg w-6/12 text-center">Our new projects are <span className='text-xl text-lime-300 border-b-2 border-lime-200'>COMING SOON</span></p>
+      <p className="text-lg w-6/12 text-center">Our new projects are <span className="text-xl text-lime-300 border-b-2 border-lime-200">COMING SOON</span></p>
     </div>
     </>
   )
